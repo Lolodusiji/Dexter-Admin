c45aa56aa54029affa0a3dcc598bbf4c7889dcae
Fatty <sholaodusiji@gmail.com>
2025-02-27 11:19:04 +0100

    update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,31 +1,26 @@
 import {
   FiUsers,
   FiDollarSign,
-  FiMail,
-  FiFileText,
-  FiSettings,
 } from "react-icons/fi";
 import { LuLogs } from "react-icons/lu";
-import { MdDashboard } from "react-icons/md";
 import { GrUserAdmin } from "react-icons/gr";
 import { IoIosLogOut } from "react-icons/io";
 import logo from "../assets/Main_Logo.svg";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const navItems = [
-  //   { icon: MdDashboard, label: "Dashboard", path: "/dashboard" },
   { icon: FiUsers, label: "Subscribers", path: "/admin/subscribers" },
   { icon: FiDollarSign, label: "Payments", path: "/admin/payments" },
   { icon: LuLogs, label: "Logs", path: "/admin/logs" },
-  //   { icon: FiFileText, label: "Content", path: "/content" },
-  { icon: GrUserAdmin, label: "Roles ", path: "/admin/roles" },
+  { icon: GrUserAdmin, label: "Roles", path: "/admin/roles" },
 ];
 
 const Sidebar = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   return (
-    <div className="fixed inset-y-0 z-[1000] w-64 bg-white shadow-xl flex flex-col items-center py-6 ">
+    <div className="fixed inset-y-0 z-[1000] w-64 bg-white shadow-xl flex flex-col items-center py-6">
       {/* Logo Section */}
       <div className="mb-12">
         <img src={logo} alt="Dexter AI Logo" width={30} className="w-[11rem]" />
@@ -38,6 +33,8 @@ const Sidebar = () => {
             key={index}
             icon={item.icon}
             label={item.label}
+            path={item.path}
+            isActive={location.pathname === item.path}
             onClick={() => navigate(item.path)}
           />
         ))}
@@ -48,6 +45,7 @@ const Sidebar = () => {
         <NavItem
           icon={IoIosLogOut}
           label="Logout"
+          isActive={false} // No active state needed for logout
           onClick={() => {
             localStorage.removeItem("accessToken");
             navigate("/");
@@ -58,10 +56,11 @@ const Sidebar = () => {
   );
 };
 
-const NavItem = ({ icon: Icon, label, onClick }) => {
+const NavItem = ({ icon: Icon, label, onClick, isActive }) => {
   return (
     <div
-      className="flex items-center gap-4 p-3 rounded-lg cursor-pointer transition hover:bg-[#F9FAFB] w-48"
+      className={`flex items-center gap-4 p-3 rounded-lg cursor-pointer transition w-48 
+        ${isActive ? "bg-[#6D68FB] text-white" : "hover:bg-[#F9FAFB]"}`}
       onClick={onClick}
     >
       <Icon className="text-xl" />
